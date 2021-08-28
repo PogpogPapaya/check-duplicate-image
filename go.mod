653afe63c54a81baddb98c3a1ac25d96042b7f6f
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-08-28 14:14:14 +0700

    INIT

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PogpogPapaya/check-duplicate-image
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	findDuplication("papaya_image/mature")
+	findDuplication("papaya_image/partiallymature")
+	findDuplication("papaya_image/unmature")
+}
+
+func findDuplication(dirPath string) {
+	log.Printf("Looking for duplication on %s\n", dirPath)
+	dirFiles, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		log.Fatalln("unable to read directory")
+	}
+
+	filesPath := make([]string, len(dirFiles))
+	for i, file := range dirFiles {
+		filePath := fmt.Sprintf("%s/%s", dirPath, file.Name())
+		filesPath[i] = filePath
+	}
+
+	sums := make(map[string][]string, len(filesPath))
+
+	for i, filePath := range filesPath {
+		log.Printf("Checking file %d out of %d\n", i, len(filesPath))
+
+		// Calculate check sum
+		file, err := os.Open(filePath)
+		if err != nil {
+			log.Fatalln("unable to open file " + filePath, err)
+		}
+
+		h := md5.New()
+		if _, err := io.Copy(h, file); err != nil {
+			log.Fatalln("unable copy file data to hash", err, filePath)
+		}
+
+		sum := h.Sum(nil)
+		hexSum := fmt.Sprintf("%x", sum)
+
+		if err := file.Close(); err != nil {
+			log.Fatalln("unable to close file", err, filePath)
+		}
+
+		if len(sums[hexSum]) != 0 {
+			sums[hexSum] = append(sums[hexSum], filePath)
+		} else {
+			sums[hexSum] = []string{filePath}
+		}
+	}
+
+	log.Println("Result")
+	dupCount := 0
+	disCount := 0
+	for _, sum := range sums {
+		count := len(sum)
+		if count >= 1 {
+			disCount++
+		}
+		if count > 1 {
+			log.Printf("%s has %d duplication\n", sum[0], count-1)
+			for i := 1; i<count; i++ {
+				log.Printf("\t%s\n", sum[i])
+			}
+			dupCount += len(sum)
+		}
+	}
+	log.Println("Total duplication: ", dupCount)
+	log.Println("Distinct: ", disCount)
+}
